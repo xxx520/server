3633f9cb304b3d643cc6d20e562a9c3cb409b422
Joas Schilling <coding@schilljs.com>
2019-02-13 14:50:06 +0100

    Fix OC.getCurrentUser() on guest pages
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- core/templates/layout.guest.php
+++ core/templates/layout.guest.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html class="ng-csp" data-placeholder-focus="false" lang="<?php p($_['language']); ?>" data-locale="<?php p($_['locale']); ?>" >
-	<head data-requesttoken="<?php p($_['requesttoken']); ?>">
+	<head data-user="<?php p($_['user_uid']); ?>" data-user-displayname="<?php p($_['user_displayname']); ?>" data-requesttoken="<?php p($_['requesttoken']); ?>">
 		<meta charset="utf-8">
 		<title>
 		<?php p($theme->getTitle()); ?>
